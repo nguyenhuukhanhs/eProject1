b6be5d97dc1ddcd22b7f8b38680292a42dd41475
nguyenhuukhanhs <nguyenhuukhanh03092003@gmail.com>
2022-06-10 16:32:33 +0700

    create

---
+++ pages/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<title>Star Organic</title>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">
+			<div class="logo">
+				<img src="../images/logo.webp" width="200px">
+			</div>
+			<div class="search">
+				<form action="" id="search-box">
+					<input type="text" name="search" placeholder="Search..." id="search-text">
+					<button type="submit" id="search-btn"><i class="fa fa-search"></i></button>
+				</form>
+			</div>
+			<div class="contact">
+				<div class="hotline">
+					<div class="hotline-icon">
+						<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+					  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+					</svg>
+					</div></a>
+					<div class="hotline-text">
+						<h6>Hỗ trợ khách hàng</h6>
+						<p>0384131xxx</p>
+					</div>
+				</div>
+				<div class="login">
+					<div class="login-icon">
+						<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+						  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+						</svg></a>
+					</div>
+					<div class="login-text">
+						<h6><a href="login.php">Đăng nhập</a></h6>
+						<p><a href="singup.php">Đăng kí</a></p>
+					</div>
+				</div>
+				<div class="cart">
+					<div class="cart-icon">
+						<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-bag-fill" viewBox="0 0 16 16">
+						  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
+						</svg></a>
+					</div>
+					<div class="cart-text">
+						<h6>Giỏ hàng</h6>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="menu">
+			<ul class="list-menu">
+				<li><a href="index.php">Trang chủ</a></li>
+				<li><a href="#">Quà tặng</a></li>
+				<li><a href="#">Thực phẩm tươi</a></li>
+				<li><a href="#">Thực phẩm khô</a></li>
+				<li><a href="#">Đồ uống</a></li>
+				<li><a href="#">Thực phẩm lạnh</a></li>
+				<li><a href="#">Chăm sóc cá nhân & Đồ dùng gia đình</a></li>
+			</ul>
+		</div>
+		<div id="main">
+
+		</div>
+		<div class="footer">
+			<p>footer</p>
+		</div>
+	</div>
+
+</body>
+</html>
